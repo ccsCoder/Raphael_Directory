d87749e72e38be745681606ff12e9c4788c98de3
Neo <ccsCoder@gmail.com>
2014-10-17 20:26:01 +0530

    Integrated Raphael

--- DirectoryRenderer.js
+++ DirectoryRenderer.js
@@ -3,32 +3,34 @@
 This class is used for rendering the basic shapes.
 */
 
-var Renderer = function() {
+function Renderer() {
 	//Init Variables.
-	this._x = undefined;
-	this._y = undefined;
 	this._maxWidth = undefined;
 	this._maxHeight = undefined;
 	this._context = undefined;
 
-	//Some Constants.
-	this._folderWidth = 50;
-	this._folderHeight= 40;
+	
 
 };
 
-Renderer.prototype.constructor = function(x,y,mW,mH,ctx) {
-	this._x = x;
-	this._y = y;
+//Renderer.prototype.constructor = function(mW,mH,ctx) {
+function Renderer(mW,mH,ctx) {
+	console.log(ctx);
 	this._maxHeight = mH;
 	this._maxWidth - mW;
 	this._context = ctx;
+	//Some Constants.
+	this._folderWidth = 50;
+	this._folderHeight= 40;
 };
 
 Renderer.prototype.createFolderIcon = function(x,y,directoryName) {
 	var folder=this._context.rect(x,y,this._folderWidth,this._folderHeight);
-	folder
-
+	folder.attr({
+		stroke: '#DCE027',
+		fill: "#DCE027",
+		"fill-opacity": 1
+	});
 
 
 }
